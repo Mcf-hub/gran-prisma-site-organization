cd5835d22b6d7ba58793757d4ec2e468f18f0ae0
Mcf-hub <ferreira.maria2322@gmail.com>
2024-10-08 17:57:45 -0300

    add adress

--- src/Components/Organisms/Contact/index.jsx
+++ src/Components/Organisms/Contact/index.jsx
@@ -13,11 +13,10 @@ const ContactSection = () => {
     window.open(`https://wa.me/${phoneNumber}`, '_blank');
   };
 
-
   return (
     <div className={styles.contactContainer}>
       <div className={styles.imageContainer}>
-        <img src={img} />
+        <img src={img} alt="Chalé principal" />
       </div>
       <div className={styles.infoContainer}>
         <h1>Entre em contato</h1>
@@ -29,17 +28,25 @@ const ContactSection = () => {
             </li>
             <li>
               <FontAwesomeIcon icon={faWhatsapp} className={styles.icon} />
-              <a href="#" onClick={handleWhatsAppClick}> (12)  99643-0031</a>
+              <a href="#" onClick={handleWhatsAppClick}> (12) 99643-0031</a>
             </li>
             <li>
               <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
               <a href="https://www.instagram.com/granprisma/" target="_blank" rel="noopener noreferrer"> @granprisma</a>
             </li>
-          <li>
-            <FontAwesomeIcon icon={faMapMarkerAlt} /> Campos do Jordão - SP
-          </li>
+            <li>
+              <FontAwesomeIcon icon={faMapMarkerAlt} className={styles.icon} />
+              <a 
+                href="https://www.google.com/maps?q=Rua%20Jose%20Anselmo%20da%20Silva,%20115,%20Vila%20Dubieux,%20Campos%20do%20Jordao" 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                Rua José Anselmo da Silva, 115, Vila Dubieux, Campos do Jordão
+              </a>
+            </li>
+          
         </ul>
-        <p className={styles.noOffice}>Não contamos com um escritório físico.</p>
+        
       </div>
     </div>
   );
